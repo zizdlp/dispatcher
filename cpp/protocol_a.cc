6238b7523359acddaaa0d6e795e3d2999cea0ace
zz <zdlp@sina.cn>
2023-11-06 17:52:46 +0800

    add_demo

--- cpp/protocol_a.cc
+++ cpp/protocol_a.cc
@@ -4,6 +4,9 @@
 template <typename T>
 bool ProtocolA::add(const int length, T* result, const T* left, const T* right) {
   std::cout << "call ProtocolA::add" << std::endl;
+  for (int index = 0; index < length; ++index) {
+    result[index] = left[index] + right[index];
+  }
   return true;
 }
 template bool ProtocolA::add<int>(const int length,
--- cpp/protocol_a_test.cc
+++ cpp/protocol_a_test.cc
@@ -1,14 +1,31 @@
 #include "protocol_a.h"
 #include <iostream>
 
+template <typename T>
+void reveal(int length, T* result) {
+  for (int i = 0; i < length; ++i) {
+    std::cout << "key:" << i << ";value:" << result[i] << " ";
+  }
+  std::cout << std::endl;
+};
+template <typename T>
+void setValue(int length, T* result) {
+  for (int i = 0; i < length; ++i) {
+    result[i] = i;
+  }
+};
+
 int main() {
   std::cout << "=============== ==================== ===============" << std::endl;
   std::cout << "=============== call protocol_a_test ===============" << std::endl;
   auto protocolA = ProtocolA();
   int length = 10;
   int* left = new int[length];
+  setValue(length, left);
   int* right = new int[length];
+  setValue(length, right);
   int* result = new int[length];
   protocolA.add(length, result, left, right);
+  reveal(length, result);
   return 0;
 }
